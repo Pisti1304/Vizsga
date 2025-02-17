20d34b08e86e18025bc74e353d58a4551cdb5ba9
Pisti1304 <72573537332@szily.hu>
2025-02-17 21:28:22 +0100

    bejelentkezes mukodik

--- bejelentkezik.php
+++ bejelentkezik.php
@@ -11,45 +11,24 @@ session_start();
 
 $hibak = [];
 if (count($_POST) > 0) {
-    $firstname = $_POST['vezeteknev'];
-    $lastname = $_POST['keresztnev'];
+   
     $jelszo = $_POST['jelszo'];
     $email = $_POST['email'];
-    $captcha=$_POST["captcha"];
-    $confirmcaptcha=$_POST["confirmcaptcha"];
-    if($captcha != $confirmcaptcha)
-    {
-        echo "<script> alert('Incorrect Captcha');</script>";
-    }
-    else
-    {
-        $row=mysqli_fetch_assoc(mysqli_query($conn,
-        "Select * from tb_user WHERE email='$email'"));
-        if(isset($row) && $jelszo == $row["jelszo"])
-        {
-            $_SESSION["id"]=$row["id"];
-            header("Location: index.html");
-        }
-        else
-        {
-            echo "<script> alert('Jelszó, vagy email rossz');</script>";
-        }
-    }
-
+  
+    
 
 
     
     $kapcsolat = kapcsolodas('mysql:host=localhost;dbname=vizsga');
 
-    $felhasznalo = ellenoriz($kapcsolat, $firstname,$lastname, $jelszo,$email);
-
+    
     if ($felhasznalo === false) {
         $hibak[] = 'Hibás adatok!';
     }
 
     if (count($hibak) === 0) {
         beleptet($felhasznalo);
-        header('Location: index.html');
+        header('Location: kapcsolat.html');
         exit();
     }
 }
