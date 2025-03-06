e662231614748273991939e2a94e62218b0f6869
JBalazsB <balazsjozan910@gmail.com>
2025-03-06 14:15:46 +0100

    Bejelentkezés - Regisztráció külön oldalra került

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    /*JELSZÓ ERŐSSÉG MÉRŐ*/
-
     var pass = document.getElementById("password");
     var pass2 = document.getElementById("password2");
     var str = document.getElementById("strength1");
@@ -11,31 +9,24 @@ document.addEventListener("DOMContentLoaded", () => {
         if (pass.value.length > 0) {
             pass.style.display = "block";
         } else {
-            pass2.style.display = "block";
+            pass.style.display = "block";
         }
         if (pass.value.length <= 0) {
             pass.style.borderColor = "white";
-            pass.style.boxShadow = "none";
             str.style.color = "white";
             str.style.display = "none";
         } else if (pass.value.length < 4) {
             pass.style.borderColor = "red";
-            pass.style.color = "red";
-            pass.style.boxShadow = "0px 2px 10px -1px red";
             str.style.color = "red";
             str.innerHTML = "A jelszavad gyenge";
             str.style.display = "block";
         } else if (pass.value.length >= 4 && pass.value.length < 8) {
             pass.style.borderColor = "yellow";
-            pass.style.color = "yellow";
-            pass.style.boxShadow = "0px 2px 10px -1px yellow";
             str.style.color = "yellow";
             str.innerHTML = "A jelszavad közepesen erős";
             str.style.display = "block";
         } else if (pass.value.length >= 8) {
             pass.style.borderColor = "rgb(59, 227, 13)";
-            pass.style.color = "rgb(59, 227, 13)";
-            pass.style.boxShadow = "0px 2px 10px -1px rgb(59, 227, 13)";
             str.style.color = "rgb(59, 227, 13)";
             str.innerHTML = "A jelszavad erős";
             str.style.display = "block";
@@ -48,43 +39,31 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             pass2.style.display = "block";
         }
-
         if (pass2.value.length <= 0) {
             pass2.style.borderColor = "white";
-            pass2.style.boxShadow = "none";
             str2.style.color = "white";
             str2.style.display = "none";
         } else if (pass2.value.length < 4) {
             pass2.style.borderColor = "red";
-            pass2.style.color = "red";
-            pass2.style.boxShadow = "0px 2px 10px -1px red";
             str2.style.color = "red";
             str2.innerHTML = "A jelszavad gyenge";
             str2.style.display = "block";
         } else if (pass2.value.length >= 4 && pass2.value.length < 8) {
             pass2.style.borderColor = "yellow";
-            pass2.style.color = "yellow";
-            pass2.style.boxShadow = "0px 2px 10px -1px yellow";
             str2.style.color = "yellow";
             str2.innerHTML = "A jelszavad közepesen erős";
             str2.style.display = "block";
         } else if (pass2.value.length >= 8) {
             pass2.style.borderColor = "rgb(59, 227, 13)";
-            pass2.style.color = "rgb(59, 227, 13)";
-            pass2.style.boxShadow = "0px 2px 10px -1px rgb(59, 227, 13)";
             str2.style.color = "rgb(59, 227, 13)";
             str2.innerHTML = "A jelszavad erős";
             str2.style.display = "block";
         }
     });
 
-
-    /*REGISZTRÁCIÓS ALERTEK*/
-
-
-    var button = document.getElementById("gomb");
-    if (button) {
-        button.addEventListener('click', function (event) {
+    var gomb = document.getElementById("gomb");
+    if (gomb) {
+        gomb.addEventListener('click', function (event) {
             var pass = document.getElementById('password').value;
             var pass2 = document.getElementById('password2').value;
 
@@ -106,18 +85,23 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     };
 
+    // function betolt(){
+    //     document.getElementById("gomb").addEventListener('click', function (event) {
+    //         var kontent2 = document.getElementById("kontent2");
+    //         var kontent3 = document.getElementById("kontent3");         
+    //         kontent2.style.display = "block";
+    //         kontent3.style.display = "none";
+    //     });
+    // }
 
-    /*BEJELENTKEZÉS VAGY REGISZTRÁCIÓ*/
 
     document.getElementById("bejelentkezes").onclick = function () {
+        var form = document.getElementById("form1");
         var kontent2 = document.getElementById("kontent2");
-        var kontent3 = document.getElementById("kontent3");
+        var kontent3 = document.getElementById("kontent3");   
         kontent2.style.display = "none";
         kontent3.style.display = "block";
 
-        kontent3.classList.remove('animacio-kezd');
-        void kontent3.offsetWidth;
-        kontent3.classList.add('animacio-kezd');
     };
 
     document.getElementById("regisztracio").onclick = function () {
@@ -126,47 +110,29 @@ document.addEventListener("DOMContentLoaded", () => {
         kontent3.style.display = "none";
         kontent2.style.display = "block";
 
-        kontent2.classList.remove('animacio-kezd');
-        void kontent2.offsetWidth;
-        kontent2.classList.add('animacio-kezd');
     };
 
-
-    /*REGISZTRÁCIÓ UTÁN FELDOBJA A BEJELENTKEZÉS ABLAKOT*/
-
-    document.getElementById("regisztracio").onclick = function () {
-        var szoveg = document.getElementById("szoveg");
-        kontent2.style.display = "none";
-        szoveg.style.display = "block";
-    };
-
-
-    /*WEBSHOP - ÁRVÁLTOZTATÓ*/
+    /*
 
     var kivalasztott = document.querySelectorAll('.kivalaszt');
-
     kivalasztott.forEach(select => {
-        select.addEventListener('change', function (event) 
-        {
-           var ar = event.target.closest('.ikon').querySelector('.ar');
+        select.addEventListener('change', function (event) {
+            var ar = event.target.closest('.ikon').querySelector('.ar');
 
             let osszeg1 = "10.000 FT";
             let osszeg2 = "20.000 FT";
             let osszeg3 = "30.000 FT";
 
-            if (event.target.value === "500 g") 
-            {
+            if (event.target.value === "500 g") {
                 ar.textContent = osszeg1;
             }
-            else if (event.target.value === "1000 g") 
-            {
+            else if (event.target.value === "1000 g") {
                 ar.textContent = osszeg2;
             }
-            else if (event.target.value === "2000 g") 
-            {
+            else if (event.target.value === "2000 g") {
                 ar.textContent = osszeg3;
             }
         });
-    });
+    });*/
 });
 
