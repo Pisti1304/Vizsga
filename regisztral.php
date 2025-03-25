ea1c7ea66591bfb965a62e574e612a554a80a087
Pisti1304 <72573537332@szily.hu>
2025-03-25 19:28:19 +0100

    felesleges sorok kiszedese

--- regisztral.php
+++ regisztral.php
@@ -2,24 +2,6 @@
 include('adatbazis.php');
 $kapcsolat = kapcsolodas('mysql:host=localhost;dbname=vizsga;charset=utf8');
 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email_check'])) {
-    $email = $_POST['email'];
-
-    
-    $sql = "SELECT id FROM felhasznalok WHERE email = :email";
-    $stmt = $kapcsolat->prepare($sql);
-    $stmt->execute([':email' => $email]);
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($result) {
-        echo "Ez az e-mail cím már regisztrálva van!";
-
-    } 
-    exit(); 
-}
-
-
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['vezeteknev'], $_POST['keresztnev'], $_POST['email'], $_POST['jelszo'])) {
     $firstname = $_POST['vezeteknev'];
     $lastname = $_POST['keresztnev'];
